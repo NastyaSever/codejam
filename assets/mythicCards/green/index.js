a4ad5d43b05c1d8f78b2240f06c9b173646af458
Nastya Severina <Severina.Nastya1992@yandex.by>
2022-08-24 23:46:19 +0300

    feat: js/css/html files add

--- assets/mythicCards/green/index.js
+++ assets/mythicCards/green/index.js
@@ -1,41 +1,132 @@
-import green1 from './green1.png';
-import green2 from './green2.png';
-import green3 from './green3.png';
-import green4 from './green4.png';
-import green5 from './green5.png';
-import green6 from './green6.png';
-import green7 from './green7.png';
-import green8 from './green8.png';
-import green9 from './green9.png';
-import green10 from './green10.png';
-import green11 from './green11.png';
-import green12 from './green12.png';
-import green13 from './green13.png';
-import green14 from './green14.png';
-import green15 from './green15.png';
-import green16 from './green16.png';
-import green17 from './green17.png';
-import green18 from './green18.png';
+// import green1 from './green1.png';
+// import green2 from './green2.png';
+// import green3 from './green3.png';
+// import green4 from './green4.png';
+// import green5 from './green5.png';
+// import green6 from './green6.png';
+// import green7 from './green7.png';
+// import green8 from './green8.png';
+// import green9 from './green9.png';
+// import green10 from './green10.png';
+// import green11 from './green11.png';
+// import green12 from './green12.png';
+// import green13 from './green13.png';
+// import green14 from './green14.png';
+// import green15 from './green15.png';
+// import green16 from './green16.png';
+// import green17 from './green17.png';
+// import green18 from './green18.png';
 
-const cards = {
-  green1,
-  green2,
-  green3,
-  green4,
-  green5,
-  green6,
-  green7,
-  green8,
-  green9,
-  green10,
-  green11,
-  green12,
-  green13,
-  green14,
-  green15,
-  green16,
-  green17,
-  green18,
-};
+const greenCards = [
+  {
+    id: 'green1',
+    cardFace: 'green1.png',
+    difficulty: 'easy',
+    color: 'green',
+  },
+  {
+    id: 'green2',
+    cardFace: 'green2.png',
+    difficulty: 'hard',
+    color: 'green',
+  },
+  {
+    id: 'green3',
+    cardFace: 'green3.png',
+    difficulty: 'hard',
+    color: 'green',
+  },
+  {
+    id: 'green4',
+    cardFace: 'green4.png',
+    difficulty: 'hard',
+    color: 'green',
+  },
+  {
+    id: 'green5',
+    cardFace: 'green5.png',
+    difficulty: 'hard',
+    color: 'green',
+  },
+  {
+    id: 'green6',
+    cardFace: 'green6.png',
+    difficulty: 'hard',
+    color: 'green',
+  },
+  {
+    id: 'green7',
+    cardFace: 'green7.png',
+    difficulty: 'normal',
+    color: 'green',
+  },
+  {
+    id: 'green8',
+    cardFace: 'green8.png',
+    difficulty: 'normal',
+    color: 'green',
+  },
+  {
+    id: 'green9',
+    cardFace: 'green9.png',
+    difficulty: 'normal',
+    color: 'green',
+  },
+  {
+    id: 'green10',
+    cardFace: 'green10.png',
+    difficulty: 'normal',
+    color: 'green',
+  },
+  {
+    id: 'green11',
+    cardFace: 'green11.png',
+    difficulty: 'normal',
+    color: 'green',
+  },
+  {
+    id: 'green12',
+    cardFace: 'green12.png',
+    difficulty: 'easy',
+    color: 'green',
+  },
+  {
+    id: 'green13',
+    cardFace: 'green13.png',
+    difficulty: 'normal',
+    color: 'green',
+  },
+  {
+    id: 'green14',
+    cardFace: 'green14.png',
+    difficulty: 'normal',
+    color: 'green',
+  },
+  {
+    id: 'green15',
+    cardFace: 'green15.png',
+    difficulty: 'normal',
+    color: 'green',
+  }, 
+  {
+    id: 'green16',
+    cardFace: 'green16.png',
+    difficulty: 'easy',
+    color: 'green',
+  },
+  {
+    id: 'green17',
+    cardFace: 'green17.png',
+    difficulty: 'easy',
+    color: 'green',
+  },
+  {
+    id: 'green18',
+    cardFace: 'green18.png',
+    difficulty: 'easy',
+    color: 'green',
+  },
 
-export default cards;
+]
+
+export default greenCards;
---
+++ script.js
@@ -0,0 +1,5 @@
+import greenCards from "./assets/mythicCards/green/index.js";
+//console.log(greenCards[6].id)
+
+const azathoth = document.querySelector('.azathoth');
+const cardmix = document.querySelector('.cardmix');
