81e8220a70357246e8f331dc162d2b8624eb8265
Anastasia <anastasia@MacBook-Pro-Aleh.local>
2022-08-29 00:16:45 +0300

    refactor: one more ancient add

--- script.js
+++ script.js
@@ -3,11 +3,17 @@ import brownCards from "./assets/mythicCards/brown/index.js";
 import blueCards from "./assets/mythicCards/blue/index.js";
 import ancients from './assets/ancients/index.js';
 
+const azathothCard = document.querySelector('.azathoth');
+const azathoth = ancients[0];
+const cthulhuCard = document.querySelector('.cthulhu');
+const cthulhu = ancients[1];
+
 function shuffleDeskOfCards() {
-  function randomGreenGards() {
+
+  function randomDiffCards(num, overalNum) {
     let randomCards = []
-    while(randomCards.length < 5) {
-      let random = Math.ceil(Math.random()*18);
+    while(randomCards.length <= num) {
+      let random = Math.ceil(Math.random()*overalNum);
       if(randomCards.includes(random)) {
         continue;
       } else {
@@ -17,35 +23,19 @@ function shuffleDeskOfCards() {
     return randomCards
   };
 
-  function randomBlueGards() {
-    let randomCards = []
-    while(randomCards.length < 2) {
-      let random = Math.ceil(Math.random()*12);
-      if(randomCards.includes(random)) {
-        continue;
-      } else {
-        randomCards.push(random)
-      }
-    };
-    return randomCards
-  };
+  let arrOfCardsGreen;
+  let arrOfCardsBrown;
+  let arrOfCardsBlue;
 
-  function randomBrownGards() {
-    let randomCards = []
-    while(randomCards.length < 9) {
-      let random = Math.ceil(Math.random()*21);
-      if(randomCards.includes(random)) {
-        continue;
-      } else {
-        randomCards.push(random)
-      }
-    };
-    return randomCards
+  if(azathothCard.style.border != '0') {
+    arrOfCardsGreen = randomDiffCards(5, greenCards.length)
+    arrOfCardsBrown = randomDiffCards(9, brownCards.length)
+    arrOfCardsBlue = randomDiffCards(2, blueCards.length)
+  } else if(cthulhuCard.style != '0') {
+    arrOfCardsGreen = randomDiffCards(4, greenCards.length);
+    arrOfCardsBrown = randomDiffCards(9, brownCards.length);
+    arrOfCardsBlue = randomDiffCards(2, blueCards.length);
   };
-
-  let arrOfCardsGreen = randomGreenGards();
-  let arrOfCardsBrown = randomBrownGards();
-  let arrOfCardsBlue = randomBlueGards();
 
   arrOfCardsBlue = arrOfCardsBlue.map(el => {
   return `blue${el}`
@@ -71,9 +61,19 @@ function shuffleDeskOfCards() {
   return arrOfCardsGreen.includes(el.id)
   });
 
-  let firstStageArr = [...arrBlue.slice(0,1),...arrBrown.slice(0,2), ...arrGreen.slice(0,1)]
-  let secondStageArr = [...arrBlue.slice(1,2),...arrBrown.slice(2,5), ...arrGreen.slice(1,3)]
-  let thirdStageArr = [...arrBrown.slice(5,9), ...arrGreen.slice(3,5)]
+  let firstStageArr;
+  let secondStageArr;
+  let thirdStageArr;
+
+  if(azathothCard.style.border != '0') {
+    firstStageArr = [...arrBlue.slice(0,1),...arrBrown.slice(0,2), ...arrGreen.slice(0,1)]
+    secondStageArr = [...arrBlue.slice(1,2),...arrBrown.slice(2,5), ...arrGreen.slice(1,3)]
+    thirdStageArr = [...arrBrown.slice(5,9), ...arrGreen.slice(3,5)]
+  } else if(cthulhuCard.style.border != '0') {
+    firstStageArr = [...arrBlue.slice(0,2),...arrBrown.slice(0,2)]
+    secondStageArr = [...arrBrown.slice(2,5), ...arrGreen.slice(0,1)]
+    thirdStageArr = [...arrBrown.slice(5,9), ...arrGreen.slice(1,4)]
+  };
 
   function shuffle(array) {
     array.sort(() => Math.random() - 0.5);
@@ -92,23 +92,51 @@ function shuffleDeskOfCards() {
 const cardmix = document.querySelector('.cardmixButton');
 const openCard = document.querySelector('.open_card');
 const closedCard = document.querySelector('.closed_card');
+const cards = document.querySelector('.cards');
+const main = document.querySelector('.main');
 const img = document.createElement('img');
 
-let firstStageGreen = ancients[0].firstStage.greenCards;
-let firstStageBlue = ancients[0].firstStage.blueCards;
-let firstStageBrown = ancients[0].firstStage.brownCards;
-let secondStageGreen = ancients[0].secondStage.greenCards;
-let secondStageBlue = ancients[0].secondStage.blueCards;
-let secondStageBrown = ancients[0].secondStage.brownCards;
-let thirdStageGreen = ancients[0].thirdStage.greenCards;
-let thirdStageBlue = ancients[0].thirdStage.blueCards;
-let thirdStageBrown = ancients[0].thirdStage.brownCards;
+let firstStageGreen;
+let firstStageBlue;
+let firstStageBrown;
+let secondStageGreen;
+let secondStageBlue;
+let secondStageBrown;
+let thirdStageGreen;
+let thirdStageBlue;
+let thirdStageBrown;
+
+if(azathothCard.addEventListener('click', ()=> {
+  main.style.display = 'flex';
+  azathothCard.style.border = 'red 2px solid';
+  cthulhuCard.style.border = '0';
+  setNumOfCardForAncients(azathoth)
+}));
+if(cthulhuCard.addEventListener('click', () => {
+  main.style.display = 'flex';
+  cthulhuCard.style.border = 'red 2px solid';
+  azathothCard.style.border = '0';
+  setNumOfCardForAncients(cthulhu);
+}));
+
+function setNumOfCardForAncients(anc) {
+  firstStageGreen = anc.firstStage.greenCards;
+  firstStageBlue = anc.firstStage.blueCards;
+  firstStageBrown = anc.firstStage.brownCards;
+  secondStageGreen = anc.secondStage.greenCards;
+  secondStageBlue = anc.secondStage.blueCards;
+  secondStageBrown = anc.secondStage.brownCards;
+  thirdStageGreen = anc.thirdStage.greenCards;
+  thirdStageBlue = anc.thirdStage.blueCards;
+  thirdStageBrown = anc.thirdStage.brownCards;
+};
 
 let shuffle;
 let newOpenCard = 0;
 let cardColor;
 
 if(cardmix.addEventListener('click', () => {
+  cards.style.display = 'flex';
   shuffle = shuffleDeskOfCards();
   newOpenCard = 0;
   openCard.style.display = 'none';
